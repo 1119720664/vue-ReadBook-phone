d06d47cf652333e5988b6e30afcc9f99c9061986
wangxu <1119720664@qq.com>
2019-01-18 11:25:38 +0800

    项目初始搭建

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,10 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'indent': 0,
+    'space-before-function-paren':0,
+     "quotes": 0
   },
   parserOptions: {
     parser: 'babel-eslint'
